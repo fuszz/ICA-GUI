cd8cae55f402f47b1b313138f7018e3499ef9c12
fussz <uliaszfilip@gmail.com>
2024-05-23 12:49:51 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QSizePolicy, QWidget
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
